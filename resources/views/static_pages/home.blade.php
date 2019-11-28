86a3f15d6f6c806359055557ed0c20b7e3865fed
miaobaobao <joychoinjapan@gmail.com>
2019-11-28 16:25:29 +0900

    init basic style

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.default')
 @section('title','ホーム')
 @section('content')
-    <h1>ホーム</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">今ご覧になるのは<a href="https://learnku.com/courses/laravel-essential-training">
+                Laravel 入門
+            </a>サンプルページ
+        </p>
+        <p>
+            早速、始めましょう！
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">今すぐアカウント作成</a>
+        </p>
+    </div>
 @stop
