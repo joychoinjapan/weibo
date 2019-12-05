af380a3825d31f05a992b629c8bb1eccf3703d3b
miaobaobao <joychoinjapan@gmail.com>
2019-12-05 16:57:55 +0900

    プロフィール

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    //新規登録画面
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    //プロフィール
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+
+
+}
