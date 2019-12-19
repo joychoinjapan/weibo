21c969bb43488a93a1ca60cf6d4bc9c07e5ea443
miaobaobao <joychoinjapan@gmail.com>
2019-12-19 18:57:32 +0900

    パスワードリセット

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -128,13 +128,12 @@ class UsersController extends Controller
     {
       $view = 'emails.confirm';
       $data = compact('user');
-      $from = 'summer@example.com';
       $name = 'Summer';
       $to = $user->email;
       $subject = "[Weibo App]会員登録を完了させてください";
 
-      Mail::send($view,$data,function ($message)use($from,$name,$to,$subject){
-          $message->from($from,$name)->to($to)->subject($subject);
+      Mail::send($view,$data,function ($message)use($name,$to,$subject){
+          $message->to($to)->subject($subject);
       });
     }
 
